bcb29b118ef5c92019ed40d44ee4a2666d58c59a
frobles03 <64452088+frobles03@users.noreply.github.com>
2022-09-08 21:44:34 -0300

    Initial commit

---
+++ templates/footer.php
@@ -0,0 +1,11 @@
+</div>
+
+<footer id="footer">
+	<div id="inner-footer">
+		Universidad de La Frontera<br>
+		Fernando Robles <br>
+		Acceso de datos 1°semetre 2022
+	</div>
+</footer>
+</body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Comunidad Hama Beads</title>
+	<link rel="stylesheet" href="assets/style.css"></head>
+<body>
+
+<nav id="site-navigation" role="navigation" class="row row-center">
+	<div class="column">
+		<h1>
+			<a href="index.php">Hama Beads</a>
+		</h1>
+		<ul class="main-menu column clearfix">
+			<li><a href="index.php">Blog</a></li>
+			<li><a href="new-post.php">Nueva Entrada</a></li>
+			<li><a href="listaC.php">Lista c</a></li>
+			<li><a href="listaCg.php">Lista cg</a></li>
+			<li><a href="listaCp.php">Lista cp</a></li>
+			<li><a href="listaCt.php">Lista ct</a></li>
+			
+		   <li><a href="mostrar_lista_creadas.php">mostrar y editar lista creadas</a></li>
+		   <li><a href="crear_lista.php">Crear Lista</a></li>
+				
+	
+
+		</ul>
+	</div>
+</nav>
+
+<div id="content" >
